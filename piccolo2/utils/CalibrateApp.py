09654602d220b643842462fcebe5c49aa81e8575
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2018-10-25 15:44:27 +0100

    display table of peaks

--- piccolo2/utils/CalibrateApp.py
+++ piccolo2/utils/CalibrateApp.py
@@ -20,6 +20,32 @@ __all__ = ['main']
 from PyQt5 import QtCore, QtGui, QtWidgets
 import calibrate_ui
 
+class Peaks(QtGui.QStandardItemModel):
+    def __init__(self,*args,**keywords):
+        self.pdata = keywords['data']
+        del keywords['data']
+        
+        QtGui.QStandardItemModel.__init__(self,*args,**keywords)
+
+
+    def selectLightSource(self,lightsource):
+        peaks = self.pdata.peaks[self.pdata.peaks.lightSource==lightsource]
+        self.clear()
+        self.setHorizontalHeaderLabels(['pixel','wavelength','spectral line'])
+        self.setColumnCount(3)
+        self.setRowCount(len(peaks))
+        i = 0
+        for pixel,row in peaks.iterrows():
+            item = QtGui.QStandardItem(str(pixel))
+            item.setEditable(False)
+            self.setItem(i,0,item)
+            item = QtGui.QStandardItem(str(self.pdata.newWavelength(pixel)))
+            item.setEditable(False)
+            self.setItem(i,1,item)
+            item = QtGui.QStandardItem(str(row.wavelength))
+            self.setItem(i,2,item)
+            i = i+1
+
 
 class CalibrateApp(QtWidgets.QMainWindow, calibrate_ui.Ui_MainWindow):
     def __init__(self, calibrationData, parent=None):
@@ -27,18 +53,23 @@ class CalibrateApp(QtWidgets.QMainWindow, calibrate_ui.Ui_MainWindow):
         self.setupUi(self)
 
         self.calibrationData = calibrationData
-
         self.calibratePlot.data = calibrationData
+        self.peaks = Peaks(data=self.calibrationData)
         
         # the light source selector
         self.lightSourceSelector.addItems(self.calibrationData.lightsources)
-        self.lightSourceSelector.currentIndexChanged.connect(self.replot)
+        self.lightSourceSelector.currentIndexChanged.connect(self.lightsourceChanged)
 
-        # plot current light source
-        self.replot()
+        # the peaks table
+        self.tableView.setModel(self.peaks)
 
-    def replot(self):
-        self.calibratePlot.plotData(self.lightSourceSelector.currentText())
+        # set the light source
+        self.lightsourceChanged()
+
+    def lightsourceChanged(self):
+        ls = self.lightSourceSelector.currentText()
+        self.calibratePlot.plotData(ls)
+        self.peaks.selectLightSource(ls)
         
 def main(calibrationData):
     app = QtWidgets.QApplication([])
